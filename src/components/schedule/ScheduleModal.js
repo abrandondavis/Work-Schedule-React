b65c174db6d34afd86dbabe34f7ab1ebfff60450
Brandon Davis <a.brandondavis@gmail.com>
2023-10-14 17:03:45 -0500

    cleaned code and fixed bugs

--- src/components/schedule/ScheduleModal.js
+++ src/components/schedule/ScheduleModal.js
@@ -13,24 +13,24 @@ export default function ScheduleModal({ isOpen, onClose, mode, initialEmployee, 
       if (mode === 'create') {
         // Call the service method to create a new employee
         console.log('handle submit inside schedulemodal.js');
-        const createdEmployee = await EmployeeService.createEmployee(formData);
-  
+
         // Pass the created employee to the callback function
-        onCreate(createdEmployee);
+        onCreate(formData);
+
       } else if (mode === 'update') {
         // Call the service method to update the employee
         const updatedEmployee = await EmployeeService.updateEmployee(initialEmployee.id, formData);
-  
+
         // Pass the updated employee to the callback function
         onUpdate(updatedEmployee);
       }
-  
+
       // Close the modal
       handleClose();
     } catch (error) {
       // Handle errors here (e.g., show an error message to the user)
       console.error('Error:', error);
-  
+
       // Optionally, you can handle specific errors or show an error message to the user
       // For example, if you want to display an error message to the user, you can use state to store the error message and render it in the component.
       // Example: setErrorState(error.message);
