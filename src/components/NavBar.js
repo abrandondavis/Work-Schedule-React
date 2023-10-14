0816da244eecc7e46aa3610acd17f5609bb88f15
abrandondavis <134955033+abrandondavis@users.noreply.github.com>
2023-10-14 15:47:30 -0500

    Update NavBar.js
    Changed brand link from # to /

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ function Navigation() {
     <nav className="navbar navbar-expand-lg bg-dark">
 
       <div className="container-fluid no-print">
-        <a className="navbar-brand text-light" href="#">Business</a>
+        <a className="navbar-brand text-light" href="/">Business</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
